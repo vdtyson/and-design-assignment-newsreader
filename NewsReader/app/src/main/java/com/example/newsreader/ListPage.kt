d0a19573f3769c51bb69566f1ba1de02ba53d25f
Versilis Tyson <VersilisTyson@gmail.com>
2019-08-15 01:41:44 -0400

    Adds ListPage Activity

--- NewsReader/app/src/main/java/com/example/newsreader/ListPage.kt
+++ NewsReader/app/src/main/java/com/example/newsreader/ListPage.kt
@@ -1,13 +1,12 @@
-package com.example.newsreader.ui
+package com.example.newsreader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.newsreader.R
 
-class SignInActivity : AppCompatActivity() {
+class ListPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        setContentView(R.layout.activity_list_page)
     }
 }
