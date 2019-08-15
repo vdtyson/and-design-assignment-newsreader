27cea45c2160a395b0886e0631722745d69e79b8
Versilis Tyson <VersilisTyson@gmail.com>
2019-08-15 17:31:31 -0400

    Adds ListPage Activity

---
+++ NewsReader/app/src/main/java/com/example/newsreader/DetailPage.kt
@@ -0,0 +1,14 @@
+package com.example.newsreader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_detail_page.*
+import kotlinx.android.synthetic.main.activity_list_page.*
+
+class DetailPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_page)
+    }
+}
