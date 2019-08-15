18fc02db9e445d040e2fa436b107ae14bdf763fa
Versilis Tyson <VersilisTyson@gmail.com>
2019-08-15 18:24:19 -0400

    Adds Note to Oncreate Detail Page

--- NewsReader/app/src/main/java/com/example/newsreader/DetailPage.kt
+++ NewsReader/app/src/main/java/com/example/newsreader/DetailPage.kt
@@ -6,7 +6,7 @@ import kotlinx.android.synthetic.main.activity_detail_page.*
 import kotlinx.android.synthetic.main.activity_list_page.*
 
 class DetailPage : AppCompatActivity() {
-
+    //Note
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_page)
